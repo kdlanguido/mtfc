6395827ad4ec96f0d00ea5366b41b03871b02e02
Lloyd Languido <79763710+Syrulli@users.noreply.github.com>
2025-02-10 00:16:53 +0800

    Forgot Password UI
    Change Image background, and forgot password form UI

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,28 +1,58 @@
-import React from "react";
+import { Box, Container } from "@mui/material";
 import ForgotPasswordForm from "./forgotPasswordForm";
-import { Box } from "@mui/joy";
-import COLORS from "@/constants/colors";
 import Image from "next/image";
 
-export default function page() {
-  return (
-    <Box
-      className="h-screen w-screen flex"
-      sx={{ background: COLORS.bg_dark_brown }}
-    >
-      <Box className="w-1/3 pt-10 px-10">
-        <ForgotPasswordForm />
-      </Box>
+export default function ForgotPassword() {
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                height: "100vh",
+                position: "relative",
+                overflow: "hidden",
+                backgroundColor: "#1D1B20", 
+            }}
+        >
+           <Box
+                sx={{
+                    flex: 1,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: "#1D1B20",
+                    padding: 4,
+                    position: "relative",
+                }}
+            >
+                <Container
+                    sx={{
+                        backgroundColor: "white",
+                        padding: 4,
+                        borderRadius: 2,
+                        boxShadow: 3,
+                        width: "400px",
+                        textAlign: "center",
+                    }}
+                >
+                    <ForgotPasswordForm />
+                </Container>
+            </Box>
 
-      <Box className="w-2/3">
-        <Image
-          className="object-cover w-auto h-full"
-          alt=""
-          src="/assets/signup.webp"
-          width={1920}
-          height={1080}
-        />
-      </Box>
-    </Box>
-  );
+            <Box
+                sx={{
+                    flex: 1,
+                    position: "relative",
+                    clipPath: "polygon(0 0, 100% 0, 100% 100%, 25% 100%)",
+                }}
+            >
+                <Image
+                    src="/assets/signup.webp"
+                    alt="Background Image"
+                    layout="fill"
+                    objectFit="cover"
+                    style={{ position: "absolute", zIndex: -1 }}
+                />
+            </Box>
+        </Box>
+    );
 }
