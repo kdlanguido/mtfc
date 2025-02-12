cc20eb4b18680f25ac2a1ab3e2d98905beb53748
Lloyd Languido <79763710+Syrulli@users.noreply.github.com>
2025-02-12 09:52:38 +0800

    Update page.tsx

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -3,56 +3,56 @@ import ForgotPasswordForm from "./forgotPasswordForm";
 import Image from "next/image";
 
 export default function ForgotPassword() {
-    return (
-        <Box
-            sx={{
-                display: "flex",
-                height: "100vh",
-                position: "relative",
-                overflow: "hidden",
-                backgroundColor: "#1D1B20", 
-            }}
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: "100vh",
+        position: "relative",
+        overflow: "hidden",
+        backgroundColor: "#1D1B20",
+      }}
+    >
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#1D1B20",
+          padding: 4,
+          position: "relative",
+        }}
+      >
+        <Container
+          sx={{
+            backgroundColor: "white",
+            padding: 4,
+            borderRadius: 2,
+            boxShadow: 3,
+            width: "400px",
+            textAlign: "center",
+          }}
         >
-           <Box
-                sx={{
-                    flex: 1,
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    backgroundColor: "#1D1B20",
-                    padding: 4,
-                    position: "relative",
-                }}
-            >
-                <Container
-                    sx={{
-                        backgroundColor: "white",
-                        padding: 4,
-                        borderRadius: 2,
-                        boxShadow: 3,
-                        width: "400px",
-                        textAlign: "center",
-                    }}
-                >
-                    <ForgotPasswordForm />
-                </Container>
-            </Box>
+          <ForgotPasswordForm />
+        </Container>
+      </Box>
 
-            <Box
-                sx={{
-                    flex: 1,
-                    position: "relative",
-                    clipPath: "polygon(0 0, 100% 0, 100% 100%, 25% 100%)",
-                }}
-            >
-                <Image
-                    src="/assets/signup.webp"
-                    alt="Background Image"
-                    layout="fill"
-                    objectFit="cover"
-                    style={{ position: "absolute", zIndex: -1 }}
-                />
-            </Box>
-        </Box>
-    );
+      <Box
+        sx={{
+          flex: 1,
+          position: "relative",
+          clipPath: "polygon(0 0, 100% 0, 100% 100%, 25% 100%)",
+        }}
+      >
+        <Image
+          src="/assets/signup.webp"
+          alt="Background Image"
+          layout="fill"
+          objectFit="cover"
+          style={{ position: "absolute", zIndex: -1 }}
+        />
+      </Box>
+    </Box>
+  );
 }
