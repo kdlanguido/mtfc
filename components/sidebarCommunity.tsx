9394220485fbcbc22eaa12ba8a48be75ef09b2d9
Lloyd Languido <79763710+Syrulli@users.noreply.github.com>
2025-02-19 12:55:19 +0800

    community dashboard cards

--- components/sidebarCommunity.tsx
+++ components/sidebarCommunity.tsx
@@ -1,35 +1,95 @@
-import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import ListItemButton from '@mui/material/ListItemButton'; // Import ListItemButton
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+"use client";
+import { useState } from "react";
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, ListItemButton, IconButton, useMediaQuery } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from "@mui/icons-material/Home";
+import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
+import ExploreIcon from "@mui/icons-material/Explore";
+import BarChartIcon from "@mui/icons-material/BarChart";
 
-const Sidebar = () => {
-    return (
-        <Drawer
-            variant="permanent"
+const menuItems = [
+    { text: "Home", icon: <HomeIcon /> },
+    { text: "Popular", icon: <LocalFireDepartmentIcon /> },
+    { text: "Explore", icon: <ExploreIcon /> },
+    { text: "All", icon: <BarChartIcon /> },
+];
+
+const CommunitySideBar = () => {
+    const [open, setOpen] = useState(false);
+    const isMobile = useMediaQuery("(max-width:900px)");
+
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
+
+    const sidebarContent = (
+        <List
             sx={{
-                width: 240,
-                flexShrink: 0,
-                '& .MuiDrawer-paper': {
-                    width: 240,
-                    boxSizing: 'border-box',
+                position: "relative",
+                "&::after": {
+                    content: '""',
+                    display: "block",
+                    marginLeft: "10px",
+                    marginTop: "10%",
+                    borderBottom: "1px solid rgb(255, 255, 255)",
                 },
             }}
         >
-            <List>
-                {['Home', 'About', 'Trainers', 'Pricing', 'Contact'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-        </Drawer>
+            {menuItems.map((item, index) => (
+                <ListItem key={item.text} disablePadding>
+                    <ListItemButton
+                        sx={{
+                            transition: "all 0.3s ease-in-out",
+                            "&:hover": {
+                                backgroundColor: "grey",
+                            },
+                        }}
+                    >
+                        <ListItemIcon sx={{ color: "white" }}>
+                            {item.icon}
+                        </ListItemIcon>
+                        <ListItemText primary={item.text} sx={{ color: "white" }} />
+                    </ListItemButton>
+                </ListItem>
+            ))}
+        </List>
+    );
+
+    return (
+        <>
+            {isMobile && (
+                <IconButton
+                    onClick={toggleDrawer}
+                    sx={{
+                        position: "absolute",
+                        top: 10,
+                        left: 10,
+                        color: "white",
+                    }}
+                >
+                    <MenuIcon />
+                </IconButton>
+            )}
+
+            <Drawer
+                variant={isMobile ? "temporary" : "permanent"}
+                open={isMobile ? open : true}
+                onClose={toggleDrawer}
+                sx={{
+                    width: isMobile ? "auto" : 240,
+                    flexShrink: 0,
+                    "& .MuiDrawer-paper": {
+                        width: 240,
+                        backgroundColor: isMobile ? "#2C2C2C" : "transparent",
+                        color: "white",
+                        paddingTop: "90px",
+                        borderRight: "none",
+                    },
+                }}
+            >
+                {sidebarContent}
+            </Drawer>
+        </>
     );
 };
-
-export default Sidebar;
+export default CommunitySideBar;
